29bb445ad4fde240e83b31e71b506c382c90b582
aguiarwalace <walace.aguiar@iudy.edu.mx>
2026-03-30 15:09:36 -0600

    celebraciones

---
+++ calendario.js
@@ -0,0 +1,12 @@
+const datosLiturgicos = {
+    "Ciclo A": [
+        { id: "a_dom_1", nombre: "1º Domingo de Adviento", cantos: [1, 2, 3, 4, 5] },
+        { id: "a_dom_2", nombre: "2º Domingo de Adviento", cantos: [2, 1, 4, 5, 3] }
+    ],
+    "Ciclo B": [
+        { id: "b_dom_1", nombre: "1º Domingo de Cuaresma", cantos: [3, 2, 5, 1, 4] }
+    ],
+    "Solemnidades": [
+        { id: "sol_inmaculada", nombre: "Inmaculada Concepción", cantos: [19, 2, 5, 4, 20] }
+    ]
+};
