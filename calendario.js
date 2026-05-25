4935097cf5896ca51caaf0b16da2c7e6e6772c3f
aguiarwalace <walace.aguiar@iudy.edu.mx>
2026-05-24 20:55:05 -0600

    Update calendario.js

--- calendario.js
+++ calendario.js
@@ -101,7 +101,7 @@ const datosLiturgicos = {
         { id: "sol_epifania", nombre: "Epifanía del Señor", cantos: [1,2,3,4,5] },
         { id: "sol_san_jose", nombre: "San José", cantos: [1,2,3,4,5] },
         { id: "sol_anunciacion", nombre: "Anunciación del Señor", cantos: [1,2,3,4,5] },
-        { id: "sol_trinidad", nombre: "Santísima Trinidad", cantos: [1,2,3,4,5] },
+        { id: "sol_trinidad", nombre: "Santísima Trinidad", cantos: [2,9] },
         { id: "sol_corpus", nombre: "Corpus Christi", cantos: [1,2,3,4,5] },
         { id: "sol_sagrado_corazon", nombre: "Sagrado Corazón de Jesús", cantos: [1,2,3,4,5] },
         { id: "sol_pedro_pablo", nombre: "San Pedro y San Pablo", cantos: [1,2,3,4,5] },
