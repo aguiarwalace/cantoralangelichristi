fa096c4e4f5084df7efa5e2e934c6deb9efdcba3
aguiarwalace <walace.aguiar@iudy.edu.mx>
2026-07-09 14:47:57 -0600

    config app

---
+++ sw.js
@@ -0,0 +1,34 @@
+// Nombre de la memoria interna (puedes cambiar el v1 a v2 cuando agregues muchos cantos nuevos)
+const CACHE_NAME = 'cantoral-angeli-v1';
+
+// Lista de archivos que el teléfono debe guardar para funcionar sin internet
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './style.css',
+    './lista_canciones.js',
+    './img/angeli_christi_logo.png',
+    './img/1000083962.png',
+    // Si tu fuente 'Thelorin' es local, asegúrate de poner su ruta exacta aquí abajo:
+    // './fonts/Thelorin.otf' 
+];
+
+// 1. INSTALACIÓN: Descarga los archivos y los guarda en el celular
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Guardando archivos en la memoria del teléfono...');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// 2. INTERCEPCIÓN: Cuando el usuario abre la app, el celular responde usando la memoria, no internet
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            // Si el archivo está en la memoria, lo usa. Si no (ej. un enlace externo), lo busca en internet.
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
